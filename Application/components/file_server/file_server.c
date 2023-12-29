ba75b35a411d7be6ee387057ba01978565689cb2
Franz Höpfinger <mail@franzhoepfinger.de>
2023-12-29 19:43:15 +0100

    Update maximum file size to 1000KB
    The commit updates the maximum file size allowed for individual files in both the `file_server.c` and `upload_script.html` files. The previous limit of 400KB has been increased to 1000KB. This change ensures that larger files can now be uploaded successfully without any issues.

--- Application/components/file_server/file_server.c
+++ Application/components/file_server/file_server.c
@@ -31,8 +31,8 @@
 
 /* Max size of an individual file. Make sure this
  * value is same as that set in upload_script.html */
-#define MAX_FILE_SIZE   (400*1024) // 400 KB
-#define MAX_FILE_SIZE_STR "400KB"
+#define MAX_FILE_SIZE   (1000*1024) // 1000 KB
+#define MAX_FILE_SIZE_STR "1000KB"
 
 /* Scratch buffer size */
 #define SCRATCH_BUFSIZE  8192
